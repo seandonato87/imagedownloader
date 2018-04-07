461c0a09624181da79d52c4fb22b7101ec523ed3
seandonato <seanvincentdonato@gmail.com>
2018-04-06 21:19:41 -0400

    Initial Commit

---
+++ ImageDownloader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageDownloader
+//
+//  Created by Sean Donato on 4/6/18.
+//  Copyright © 2018 Sean Donato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageDownloader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ImageDownloader
+//
+//  Created by Sean Donato on 4/6/18.
+//  Copyright © 2018 Sean Donato. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
